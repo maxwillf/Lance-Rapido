82a9ec9eb37b8e8c20a72e1e781fc839b4bfd48f
WilliamCDL <42789470+WilliamCDL@users.noreply.github.com>
2021-02-17 13:55:41 -0300

    ControllerProduct
    apenas os metodos get e save

---
+++ src/main/java/com/leilao/lance/rapido/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.leilao.lance.rapido.controller;
+
+
+
+import com.leilao.lance.rapido.model.Product;
+import com.leilao.lance.rapido.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+    private final ProductService productService;
+
+
+    public ProductController(ProductService productService) { this.productService = productService;}
+
+    @PostMapping("/product")
+    Product newProduct(@RequestBody Product newProduct){
+        return productService.saveProduct(newProduct);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductByID(@PathVariable int productID) {
+        return productService.findProduct(productID);
+    }
+
+
+
+}
--- src/main/java/com/leilao/lance/rapido/service/ProductService.java
+++ src/main/java/com/leilao/lance/rapido/service/ProductService.java
@@ -1,7 +1,26 @@
 package com.leilao.lance.rapido.service;
 
+import com.leilao.lance.rapido.model.Product;
+import com.leilao.lance.rapido.repository.ProductRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository ;
+    }
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> findProduct(int productID){
+        return Optional.ofNullable(productRepository.findByProductID(productID));
+    }
+
+
 }
